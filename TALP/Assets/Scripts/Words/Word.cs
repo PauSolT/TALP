052a564c4f06f03a2ef62bb4dd7414c108b1849e
pau <pausoltor@gmail.com>
2024-03-01 14:24:15 +0100

    Fix Learning bug

--- TALP/Assets/Scripts/Words/Word.cs
+++ TALP/Assets/Scripts/Words/Word.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Word 
 {
+[SerializeField]
     public string japanese;
+[SerializeField]
     public string kanji;
+[SerializeField]
     public string word;
+[SerializeField]
     public string extra;
 
     public Word(string jap, string kan, string wor, string ext)
